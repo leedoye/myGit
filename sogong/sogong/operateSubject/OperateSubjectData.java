40822f67b02746b3e12613128e200d57a90022d7
LDY\이도예 <phoenix945@naver.com>
2016-12-04 01:09:59 +0900

    no message

---
+++ sogong/sogong/operateSubject/OperateSubjectData.java
@@ -0,0 +1,17 @@
+package operateSubject;
+
+public class OperateSubjectData {
+
+	public String subjectProcess;
+	public String target;
+	public String feature;
+	public int recruitNumber;
+	public int tuition;
+	public String subjectCode;
+	public String purpose;
+	public String changeRecord;
+	public String subjectName;
+	public int totalDate;
+	public int totalHour;
+	public int lectureHour;
+}
